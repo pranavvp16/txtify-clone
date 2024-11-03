d358ceeb6682cf52d5c38246ad7508ce2cb8dabb
Pranav <pranavprajapati586@gmail.com>
2024-11-03 18:25:50 +0530

    Scrap using newspaper3k

--- txtify/txtify.py
+++ txtify/txtify.py
@@ -1,55 +1,44 @@
-
+from newspaper import Article, languages
 
 allow_tags = [
-            'p', 'br',
+            'p', 'br', 'a',
             'em', 'i', 'tt', 'code', 'pre', 'blockquote', 'h1',
             'h2', 'h3', 'h4', 'h5', 'h6',
             'ul', 'ol', 'li', 'dl', 'dt', 'dd'
 ]
 
 
-class Txtify:
+class Txtify(Article):
     def __init__(self, url):
+
+        super().__init__(url, language='en')
         self.url = url
         self.soup = None
         self.content = None
 
+
     def download_content(self):
-        import requests
+        self.download()
+        print(self.html)
 
-        response = requests.get(self.url)
-        response.raise_for_status()
-        self.content = response.content
 
-    def parse_html(self, content):
-        from bs4 import BeautifulSoup
+    def parse_html(self):
+        self.parse()
 
-        self.soup = BeautifulSoup(content, "lxml")
-
-    def extract_content(self, soup):
+    def extract_content(self):
         import regex as re
+        title = self.title
+        content = title + "\n" + self.text
 
-        main_content = self.soup.find("article")
-        if main_content is None:
-            main_content = soup.find("div", class_="content")  # Adjust class as needed
-        if main_content is None:
-            main_content = soup.find("main")
-
-        if main_content:
-            content = main_content.find_all(allow_tags)
-            content = "\n".join(tag.get_text(strip=True) for tag in content)
-
-            self.content = "\n".join(re.findall(r'.{1,70}(?:\s+|$)', content))
-
-        else:
-            self.content = "Invalid URL"
+        # regex to display text in same format as txtify
+        self.content = "\n".join(re.findall(r'.{1,70}(?:\s+|$)', content))
 
         return self.content
 
     def run(self):
         self.download_content()
-        self.parse_html(self.content)
+        self.parse_html()
 
-        return self.extract_content(self.soup)
+        return self.extract_content()
 
 
